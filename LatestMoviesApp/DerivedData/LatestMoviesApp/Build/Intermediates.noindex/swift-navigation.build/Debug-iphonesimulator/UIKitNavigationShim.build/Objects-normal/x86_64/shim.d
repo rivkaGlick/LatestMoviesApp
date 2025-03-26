9aed72d067e5896c8f0adfc17a993f066f161b67
rivka <rivki311@gmail.com>
2025-03-27 01:59:11 +0200

    remove unneed files

--- LatestMoviesApp/DerivedData/LatestMoviesApp/Build/Intermediates.noindex/swift-navigation.build/Debug-iphonesimulator/UIKitNavigationShim.build/Objects-normal/x86_64/shim.d
+++
@@ -1,6 +0,0 @@
-dependencies: \
-  /Users/rivkiglick/Desktop/LatestMoviesApp/DerivedData/LatestMoviesApp/SourcePackages/checkouts/swift-navigation/Sources/UIKitNavigationShim/shim.m \
-  /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator.sdk/usr/include/TargetConditionals.modulemap \
-  /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator.sdk/System/Library/Frameworks/UIKit.framework/Modules/module.modulemap \
-  /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator.sdk/usr/include/ObjectiveC.modulemap \
-  /Users/rivkiglick/Desktop/LatestMoviesApp/DerivedData/LatestMoviesApp/SourcePackages/checkouts/swift-navigation/Sources/UIKitNavigationShim/include/shim.h
--- LatestMoviesApp/DerivedData/LatestMoviesApp/Build/Intermediates.noindex/swift-syntax.build/Debug/_SwiftSyntaxCShims.build/Objects-normal/arm64/dummy.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/rivkiglick/Desktop/LatestMoviesApp/DerivedData/LatestMoviesApp/SourcePackages/checkouts/swift-syntax/Sources/_SwiftSyntaxCShims/include/module.modulemap \
-  /Users/rivkiglick/Desktop/LatestMoviesApp/DerivedData/LatestMoviesApp/SourcePackages/checkouts/swift-syntax/Sources/_SwiftSyntaxCShims/dummy.c
